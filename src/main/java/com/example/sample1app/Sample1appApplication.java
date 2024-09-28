3e96645a9e6279cc9330df3f6a2a3888a7b2951c
maru1019 <green101927.10@gmail.com>
2024-09-28 23:41:05 +0900

    初回のコミット

---
+++ src/main/java/com/example/sample1app/Sample1appApplication.java
@@ -0,0 +1,13 @@
+package com.example.sample1app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sample1appApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Spring");
+	}
+
+}
