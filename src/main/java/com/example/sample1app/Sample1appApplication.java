a0e3faf597a0dc6dad8a62397b06934b5f0d6142
maru1019 <green101927.10@gmail.com>
2024-09-29 01:07:08 +0900

    first commit

--- src/main/java/com/example/sample1app/Sample1appApplication.java
+++ src/main/java/com/example/sample1app/Sample1appApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Sample1appApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring");
+		SpringApplication.run(Sample1appApplication.class, args);
 	}
 
 }
