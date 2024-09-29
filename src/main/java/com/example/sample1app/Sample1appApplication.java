a72be7fd4ce1f7ade80de458331e990c5a1d904a
maru1019 <green101927.10@gmail.com>
2024-09-29 22:05:50 +0900

    ApplicationRunnerでアプリケーションプログラムを作成

--- src/main/java/com/example/sample1app/Sample1appApplication.java
+++ src/main/java/com/example/sample1app/Sample1appApplication.java
@@ -1,14 +1,16 @@
 package com.example.sample1app;
 
+import java.util.Arrays;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.Banner.Mode;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class Sample1appApplication implements CommandLineRunner {
+public class Sample1appApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(Sample1appApplication.class);
@@ -17,10 +19,12 @@ public class Sample1appApplication implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String[] args) {
+	public void run(ApplicationArguments args) {
 		System.out.println("+--------------------------------------+");
-		System.out.println("| this is CommandLine Runner program. |");
+		System.out.println("| this is Application Runner program. |");
 		System.out.println("+--------------------------------------+");
-		System.out.println("[" + String.join(", ",args) + "]");
+		System.out.println(args.getOptionNames());
+		System.out.println(args.getNonOptionArgs());
+		System.out.println(Arrays.asList(args.getSourceArgs()));
 	}
 }
