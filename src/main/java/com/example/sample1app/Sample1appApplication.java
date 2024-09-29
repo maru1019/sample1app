c45be028c6b03665b00caf8ca2cd327d92072b9a
maru1019 <green101927.10@gmail.com>
2024-09-29 18:01:12 +0900

    Springバナーの表示OFF

--- src/main/java/com/example/sample1app/Sample1appApplication.java
+++ src/main/java/com/example/sample1app/Sample1appApplication.java
@@ -2,12 +2,14 @@ package com.example.sample1app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.Banner.Mode;
 
 @SpringBootApplication
 public class Sample1appApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring");
+		SpringApplication app = new SpringApplication(Sample1appApplication.class);
+		app.setBannerMode(Mode.OFF);
+		app.run(args);
 	}
-
 }
