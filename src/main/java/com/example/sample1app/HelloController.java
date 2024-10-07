071a90fac667041abb96be4ce8410fe4db67e8f2
maru1019 <green101927.10@gmail.com>
2024-10-07 21:57:41 +0900

    forwardとredirect実装

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class HelloController {
@@ -13,35 +11,17 @@ public class HelloController {
   @RequestMapping(value="/", method=RequestMethod.GET)
   public ModelAndView index(ModelAndView mav) {
     mav.setViewName("index");
-    mav.addObject("mdg","フォームを送信してください");
+    mav.addObject("msg","HelloController/indecxのページです");
     return mav;
   }
 
-  @RequestMapping(value="/", method=RequestMethod.POST)
-  public ModelAndView form(
-    @RequestParam(value="check1",required=false)boolean check1, 
-    @RequestParam(value="radio1",required=false)String radio1,
-    @RequestParam(value="select1",required=false)String select1,
-    @RequestParam(value="select2",required=false)String[] select2,
-    ModelAndView mav) {
+  @RequestMapping("/other")
+  public String other() {
+    return "redirect:/";
+  }
 
-    String res = "";
-    try {
-      res = "check:" + check1 +
-      " radio:" + radio1 +
-      " select:" + select1 +
-      " select2:";
-    } catch (NullPointerException e) {}
-    try {
-      res += select2[0];
-      for(int i = 1;i < select2.length;i++) {
-        res += ", " + select2[i];
-      }
-    } catch (NullPointerException e) {
-      res += "null";
-    }
-    mav.addObject("msg",res);
-    mav.setViewName("index");
-    return mav;
+  @RequestMapping("/home")
+  public String home() {
+    return "forward:/";
   }
 }
