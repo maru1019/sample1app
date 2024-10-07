59b7cbbcf7d8fe0552b8067eaabe075ed68dbff2
maru1019 <green101927.10@gmail.com>
2024-10-08 00:24:56 +0900

    th:swichによるスイッチ処理

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -3,16 +3,16 @@ package com.example.sample1app;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HelloController {
-  @RequestMapping("/")
-  public ModelAndView index(ModelAndView mav) {
+  @RequestMapping("/{month}")
+  public ModelAndView index(@PathVariable int month, ModelAndView mav) {
     mav.setViewName("index");
-    mav.addObject("msg", "データを表示します。");
+    mav.addObject("msg", month + "月は？");
     String[] data = new String[] {"One","Two","Three"};
-    mav.addObject("data", data);
+    mav.addObject("month", month);
     return mav;
   }
 }
