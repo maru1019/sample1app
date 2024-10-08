b084d5dc413bbb60226692677810ef45df2a24c0
maru1019 <green101927.10@gmail.com>
2024-10-08 22:36:00 +0900

    Mustache実装

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -3,15 +3,13 @@ package com.example.sample1app;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HelloController {
-  @RequestMapping("/{month}")
-  public ModelAndView index(@PathVariable int month, ModelAndView mav) {
+  @RequestMapping("/")
+  public ModelAndView index(ModelAndView mav) {
     mav.setViewName("index");
-    mav.addObject("msg", month + "月は？");
-    mav.addObject("month", month);
+    mav.addObject("msg", "メッセージだよ。");
     return mav;
   }
 }
