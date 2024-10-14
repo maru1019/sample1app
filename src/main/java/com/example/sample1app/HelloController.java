dd0b68afe33e5e3c7ba6f6d5c9d092a57a70c7f0
maru1019 <green101927.10@gmail.com>
2024-10-14 16:46:03 +0900

    フォーム実装、CRUDを作成

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -1,12 +1,19 @@
 package com.example.sample1app;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.sample1app.entity.Person;
 import com.example.sample1app.repository.PersonRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 public class HelloController {
@@ -15,12 +22,19 @@ public class HelloController {
   PersonRepository repository;
 
   @RequestMapping("/")
-  public ModelAndView index(ModelAndView mav) {
+  public ModelAndView index(@ModelAttribute("formModel") Person person, ModelAndView mav) {
     mav.setViewName("index");
     mav.addObject("title","Hello page");
     mav.addObject("msg", "this is JPA sample data.");
-    Iterable<Person> list = repository.findAll();
+    List<Person> list = repository.findAll();
     mav.addObject("data",list);
     return mav;
+  }
+
+  @RequestMapping(value = "/", method = RequestMethod.POST)
+  @Transactional
+  public ModelAndView form(@ModelAttribute("formModel") Person person, ModelAndView mav) {
+    repository.saveAndFlush(person);
+    return new ModelAndView("redirect:/");
   }
 }
