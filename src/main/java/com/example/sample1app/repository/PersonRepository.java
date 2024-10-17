73e8ca8d3d7734016bbab016a593f4a986dce508
maru1019 <green101927.10@gmail.com>
2024-10-17 20:49:01 +0900

    JpaRepositoryのメソッド実装

--- src/main/java/com/example/sample1app/repository/PersonRepository.java
+++ src/main/java/com/example/sample1app/repository/PersonRepository.java
@@ -1,6 +1,6 @@
 package com.example.sample1app.repository;
 
-
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +11,10 @@ import com.example.sample1app.entity.Person;
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
   public Optional<Person> findById(Long name);
+  public List<Person> findByNameLike(String name);
+  public List<Person> findByIdIsNotNullOrderByIdDesc();
+  public List<Person> findByAgeGreaterThan(Integer age);
+  public List<Person> findByAgeBetween(Integer age1,Integer age2);
+
+
 }
