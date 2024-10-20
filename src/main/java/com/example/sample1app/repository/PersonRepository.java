49f85f414b4462c7f9481ea0ce45f8172bc93958
maru1019 <green101927.10@gmail.com>
2024-10-20 17:18:48 +0900

    @Query実装

--- src/main/java/com/example/sample1app/repository/PersonRepository.java
+++ src/main/java/com/example/sample1app/repository/PersonRepository.java
@@ -4,17 +4,20 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.sample1app.entity.Person;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+  @Query("SELECT d FROM Person d ORDER BY d.name")
+  List<Person> findAllOrderByName();
+
   public Optional<Person> findById(Long name);
   public List<Person> findByNameLike(String name);
   public List<Person> findByIdIsNotNullOrderByIdDesc();
   public List<Person> findByAgeGreaterThan(Integer age);
   public List<Person> findByAgeBetween(Integer age1,Integer age2);
-
-
 }
