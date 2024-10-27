6d7d980303c294ad6d1d75765e91b3165cbfb2d0
maru1019 <green101927.10@gmail.com>
2024-10-27 16:40:48 +0900

    修正

--- src/main/java/com/example/sample1app/SampleService.java
+++ src/main/java/com/example/sample1app/SampleService.java
@@ -3,14 +3,14 @@ package com.example.sample1app;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
-import com.example.sample1app.entities.Post;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.sample1app.repositories.PostRepository;
+import com.example.sample1app.entities.Post;
+
 
 @Service
 public class SampleService {
-
   private String baseUrl = "https://jsonplaceholder.typicode.com/posts";
 
   @Autowired
